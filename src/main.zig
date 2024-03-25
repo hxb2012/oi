d74a92f54e6855c098aa13b05c33e7878068360b
洪筱冰 <hxb@localhost.localdomain>
2024-03-25 19:12:15 +0800

    oi: 导出std.math.order

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,10 @@
-pub const io = @import("io.zig");
+const std = @import("std");
+pub const order = std.math.order;
 
+pub const io = @import("io.zig");
 pub const printf = io.printf;
 pub const scanf = io.scanf;
 
 test {
-    const std = @import("std");
     std.testing.refAllDecls(@This());
 }
